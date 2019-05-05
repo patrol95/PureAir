a861de998f072f2ef7da42c14ddda70086509206
Wojciech Juskowiak <juski552@gmail.com>
2019-05-05 17:25:02 +0200

    Create and Connect Fragments

---
+++ app/src/main/java/com/teamnumberb/pureair/DirectionsFragment.java
@@ -0,0 +1,19 @@
+package com.teamnumberb.pureair;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DirectionsFragment extends Fragment {
+
+    public static DirectionsFragment newInstance() { return new DirectionsFragment(); }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_directions, null);
+    }
+}
